9acf792f351936f15a5565c8936c42a54eaf0509
Yernar <dvayernar@gmail.com>
2024-12-20 21:25:46 +0500

    inital fix

--- app/src/main/java/com/example/booksstoreapp/view/adapter/DataBindHolder.kt
+++ app/src/main/java/com/example/booksstoreapp/view/adapter/DataBindHolder.kt
@@ -1,9 +1,7 @@
 package com.example.booksstoreapp.view.adapter
 
-
-import android.databinding.ViewDataBinding
-import android.support.v7.widget.RecyclerView
-
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
 
 class DataBindHolder<out T : ViewDataBinding> constructor(val binding: T) :
     RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/example/booksstoreapp/view/home/MainFragment.kt
+++ app/src/main/java/com/example/booksstoreapp/view/home/MainFragment.kt
@@ -1,4 +1,27 @@
 package com.example.booksstoreapp.view.home
 
-class MainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.booksstoreapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
